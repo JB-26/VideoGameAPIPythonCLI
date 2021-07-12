bb610075f1016b6f3469deaad7a61682cea51ed5
Joshua Blewitt <joshblewitt@protonmail.com>
2021-07-13 00:22:03 +0100

    New functions and refactoring

--- main.py
+++ main.py
@@ -14,24 +14,43 @@ def main():
     access_token = login()
     print(f"Logged in successfully! Your token is: {access_token}")
     while True:
-        print('Type the letter in the brackets to access the API')
-        print('Main menu:\n(D)isplay games\n(F)ind game\n(U)pdate game\n(A)dd game\n(D)elete game\n(Q)uit')
+        print('Type the letters in the brackets to access the API')
+        print('Main menu:\n(D)isplay games\n(F)ind game\n(U)pdate game\n(A)dd game\n(De)lete game\n(Q)uit')
         choice = input("Please enter your choice").upper()
         if choice == 'D':
             display_game(access_token)
         elif choice == 'F':
             find_game(access_token)
         elif choice == 'U':
-            print('Update')
+            update_game(access_token)
         elif choice == 'A':
-            print('Add')
-        elif choice == 'D':
-            print('Delete')
+            add_game(access_token)
+        elif choice == 'DE':
+            delete_game(access_token)
         elif choice == 'Q':
             print('Goodbye!')
             break
         else:
             print('Invalid command - please try again!')
+
+
+def decode_json(json_data):
+    """
+    :param json_data:
+    :return:
+    Decodes JSON response from API
+    """
+    # decode JSON
+    json_response = json_data.json()
+    games = json_response.get('Games')
+
+    # iterate through response
+    for i in range(len(games)):
+        temp_dict = games[i]
+        game = temp_dict['Game']
+        for x, y in game.items():
+            print(f'{x}:{y}')
+        print('\n')
 
 
 def login():
@@ -51,12 +70,12 @@ def login():
         })
 
         # construct headers
-        headers = {
+        headers_login = {
             'Content-Type': 'application/json'
         }
 
         print('Now logging into the Video Game API\nPlease wait (the API might be slow to respond at first)')
-        r = requests.post(f'{apiUrl}/login', headers=headers, data=payload)
+        r = requests.post(f'{apiUrl}/login', headers=headers_login, data=payload)
         if r.status_code != 200:
             print('Error with logging in - please try again!')
             pass
@@ -80,17 +99,7 @@ def display_game(access_token):
     }
 
     r = requests.get(f'{apiUrl}/displayGames', headers=headers)
-    # decode JSON
-    json_response = r.json()
-    games = json_response.get('Games')
-
-    # iterate through response
-    for i in range(len(games)):
-        temp_dict = games[i]
-        game = temp_dict['Game']
-        for x, y in game.items():
-            print(f'{x}:{y}')
-        print('\n')
+    decode_json(r)
 
 
 def find_game(access_token):
@@ -110,19 +119,113 @@ def find_game(access_token):
     })
 
     r = requests.get(f'{apiUrl}/findGame', headers=headers, data=payload)
-    if r.status_code != 200:
-        print('No matches found!')
-    else:
-        # decode JSON
-        json_response = r.json()
-        games = json_response.get('Games')
+    decode_json(r)
 
-        for i in range(len(games)):
-            temp_dict = games[i]
-            game = temp_dict['Game']
-            for x, y in game.items():
-                print(f'{x}:{y}')
-            print('\n')
+
+def update_game(access_token):
+    """
+    :return: Updates a game in the database
+    """
+    update_id = int(input('Enter the ID of the game you want to update'))
+    update_field = input('Enter the field that you wish to update (i.e. Name, Genre, Platform').lower()
+    update_value = input('Enter the value you want to update with')
+
+    # construct payload for request
+    payload = json.dumps({
+        "id": update_id,
+        "field": update_field,
+        "value": update_value
+    })
+
+    # construct headers
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    print(f'Now updating - \nID: {update_id}\nField: {update_field}\nValue: {update_value}')
+    r = requests.put(f'{apiUrl}/updateGame', headers=headers, data=payload)
+    if r.status_code != 200:
+        print('An error occurred - please try again!')
+    else:
+        print('Update complete!')
+        json_response = r.json()
+        result = json_response.get('Game updated:')
+        result = result.get('Game')
+        print('New updated entry:')
+        for x in result:
+            print(f'{x} {result[x]}')
+
+
+def delete_game(access_token):
+    """
+    :return:
+    Deletes a game from the database
+    """
+    delete_id = int(input('Enter the ID of the game in the database you want to delete'))
+    # construct payload for request
+    payload = json.dumps({
+        "id": delete_id
+    })
+
+    # construct headers
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    r = requests.delete(f'{apiUrl}/deleteGame', headers=headers, data=payload)
+    print('The following game has been deleted:')
+    if r.status_code != 200:
+        print('An error occurred - please try again!')
+    else:
+        print('Update complete!')
+        json_response = r.json()
+        result = json_response.get('The following has been deleted')
+        result = result.get('Game')
+        print('New updated entry:')
+        for x in result:
+            print(f'{x} {result[x]}')
+
+
+def add_game(access_token):
+    """
+    :param access_token:
+    :return: Adds a game to the database
+    """
+
+    # construct headers
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    print('Please enter the following details for the game you want to add:')
+    add_name = input('Enter the name: ')
+    add_platform = input('Enter the platform: ')
+    add_publisher = input('Enter the publisher: ')
+    add_genre = input('Enter the genre: ')
+    add_year = int(input('Enter the year: '))
+
+    # construct payload for request
+    payload = json.dumps({
+        "name": add_name,
+        "platform": add_platform,
+        "publisher": add_publisher,
+        "genre": add_genre,
+        "year": add_year
+    })
+
+    r = requests.post(f'{apiUrl}/addGame', headers=headers, data=payload)
+    if r.status_code != 201:
+        print('An error has occurred - please try again!')
+    else:
+        print('The following has been added:')
+        print(f'Name: {add_name}')
+        print(f'Platform: {add_platform}')
+        print(f'Publisher: {add_publisher}')
+        print(f'Genre: {add_genre}')
+        print(f'Year: {add_year}')
 
 
 if __name__ == '__main__':
